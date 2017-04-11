2f52ae7ff5322fde9328e5ac68db831524bef22a
Ben Smith <bsmith@apl.washington.edu>
2017-04-11 07:36:51 -0700

    read_sim_ATL03.m: work on reading the right fields

--- read_sim_ATL03.m
+++ read_sim_ATL03.m
@@ -1,4 +1,4 @@
-function [D, params]=read_sim_ATL03(filename, pairs)
+function [H, D, params]=read_sim_ATL03(filename, pairs)
 
 if ~exist('pairs','var')
     pairs=1:3;
@@ -16,7 +16,9 @@ out_struct.geolocation=struct(...
     'surf_type', [], ...
     'ph_index_beg', [], ...
     'delta_time', [], ...
-    'segment_ph_cnt', []);
+    'segment_ph_cnt', [], ...
+    'sigma_across', [], ...
+    'sigma_along', []);
 out_struct.heights=struct(...
     'delta_time', [], ...
     'dist_ph_across', [], ...
@@ -26,6 +28,8 @@ out_struct.heights=struct(...
     'lon_ph', [], ...
     'pce_mframe_cnt', [], ...
     'ph_id_count', [], ...
+    'ph_id_pulse', [], ...
+    'ph_id_channel',[], ...
     'signal_conf_ph', []);
 out_struct.bckgrd_atlas=struct('bckgrd_rate', [],'pce_mframe_cnt',[]);
 
@@ -44,28 +48,58 @@ for kT=pairs(:)'%length(GT)
                 D(beam).(F0{k0}).(F1{k1})=h5read(filename, fieldName);
             end
         end  
-        %fprintf(1, '%s:\t%8.0d\t%8.0d\n',  GT_grp, length(D(beam).heights.h_ph), size(D(beam).heights.signal_conf_ph,2));
+        D(beam).geolocation=index_struct(D(beam).geolocation, D(beam).geolocation.ph_index_beg ~=0);
+        
+        
+         if strcmp(deblank(h5readatt(filename, GT_grp,'atlas_beam_type')),'strong')
+            params(beam).N_det=16;
+        else
+            params(beam).N_det=4;
+        end
+        params(beam).RGT=h5read(filename,'/ancillary_data/start_rgt');
+        params(beam).orbit=h5read(filename,'/ancillary_data/start_orbit');
+        params(beam).cycle=h5read(filename,'/ancillary_data/start_cycle');
+        params(beam).GT=beam;
+        params(beam).spot_number=str2double(deblank(h5readatt(filename, GT_grp,'atlas_spot_number')));
+        params(beam).PT=str2double(kT);
     end
 end
 
 
+
+
+for k=1:length(D) 
+    H(k)=D(k).heights;
+end
+
 for beam=beams(:)' %length(D)
     % extract the ice-sheet column for the signal confidence
-    D(beam).heights.signal_conf_ph=D(beam).heights.signal_conf_ph(4,:)';
+    H(beam).signal_conf_ph=D(beam).heights.signal_conf_ph(4,:)';
     
-    [D(beam).heights.ph_seg_num, ...
-        D(beam).heights.x_RGT]=deal(NaN(size(D(1).heights.h_ph)));
-    for k=1:length(D(beam).geolocation.ph_index_beg)-1
-        ind_range=[D(beam).geolocation.ph_index_beg(k), D(beam).geolocation.ph_index_beg(k+1)-1];
+    [H(beam).ph_seg_num, ...
+        H(beam).x_RGT, ...
+        H(beam).seg_dist_x, ...
+        H(beam).ph_seg_num, ...
+        H(beam).BGR, ...
+        H(beam).sigma_across, ...
+        H(beam).sigma_along]=deal(NaN(size(D(beam).heights.h_ph)));
+    for k=1:length(D(beam).geolocation.ph_index_beg);
+        ind_range=D(beam).geolocation.ph_index_beg(k)+int64([0 D(beam).geolocation.segment_ph_cnt(k)-1]);
         if all(ind_range~=0)
-            D(beam).heights.ph_seg_num(ind_range(1):ind_range(2))=k;
+            H(beam).ph_seg_num(ind_range(1):ind_range(2))=k;
         end
     end
-    D(beam).heights.ph_seg_num(D(beam).geolocation.ph_index_beg(end):end)=k+1;
-    these=isfinite(D(beam).heights.ph_seg_num); 
-    D(beam).heights.x_RGT(these)=D(beam).geolocation.segment_dist_x(D(beam).heights.ph_seg_num(these));%+D(beam).heights.dist_ph_along(these);
-    good=isfinite(D(beam).heights.pce_mframe_cnt & D(beam).heights.pce_mframe_cnt & D(beam).heights.pce_mframe_cnt < length(D(beam).bckgrd_atlas.bckgrd_rate));
-    D(beam).BGR(good)=D(beam).bckgrd_atlas.bckgrd_rate(D(beam).heights.pce_mframe_cnt(good));
+    these=isfinite(H(beam).ph_seg_num);
+    H(beam).seg_dist_x(these)=D(beam).geolocation.segment_dist_x(H(beam).ph_seg_num(these));
+    H(beam).sigma_across(these)=D(beam).geolocation.sigma_across(H(beam).ph_seg_num(these));
+    H(beam).sigma_along(these)=D(beam).geolocation.sigma_along(H(beam).ph_seg_num(these));
+        
+    H(beam).x_RGT(these)=D(beam).geolocation.segment_dist_x(H(beam).ph_seg_num(these))+H(beam).dist_ph_along(these);
+    good=isfinite(H(beam).x_RGT) & H(beam).pce_mframe_cnt>0 & H(beam).pce_mframe_cnt < length(D(beam).bckgrd_atlas.bckgrd_rate);
+    H(beam).BGR(good)=D(beam).bckgrd_atlas.bckgrd_rate(H(beam).pce_mframe_cnt(good));
+    H(beam).pulse_num=200*double(H(beam).pce_mframe_cnt)+double(H(beam).ph_id_pulse);
+    H(beam).beam=zeros(size(H(beam).h_ph))+beam;
+    H(beam).h_ph=double(H(beam).h_ph);
 end
 
 
@@ -79,8 +113,8 @@ end
 % example plot:
 
 if false
-    thefile='/Volumes/ice1/ben/sdt/KTL03/KTL03s_for_ben/ATL03_sim_photons_allbeams_seg2_rgt696_v5.h5';
-    D=read_sim_ATL03(thefile);
+    filename='/Volumes/ice1/ben/sdt/KTL03/ATL03forATL06version1b.h5';
+     D=read_sim_ATL03(filename);
      for kB=1:2
         temp=D(kB).heights;
         temp=index_struct(temp, 1:100:length(temp.h_ph));
