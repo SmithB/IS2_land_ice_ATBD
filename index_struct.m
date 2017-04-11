084b41362a3a541fbc054294a7cee1bb78c03c99
Ben Smith <bsmith@apl.washington.edu>
2017-04-11 07:36:01 -0700

    index_struct.m: Better detection of skinny/fat arrays

--- index_struct.m
+++ index_struct.m
@@ -7,18 +7,21 @@ function S1=index_struct(S, els, f);
 % the output structure contains only those fields from S.
 
 
-if ~exist('f','var');
+if ~exist('f','var')
     f=fieldnames(S);
 end
-for k=1:length(f);
-    eval(['F=size(S.', f{k},');']);
+for k=1:length(f)
+    %eval(['F=size(S.', f{k},');']);
+    F=size(S.(f{k}));
     if any(F==0)
         eval(['S1.', f{k},'=[];']);
         continue
     end
-    if F(2)>1 && length(els(:))==F(1);
-        eval(['S1.', f{k},'=S.',f{k},'(els,:);']);
+    if F(2)>1 && length(els(:))==F(1)
+        %eval(['S1.', f{k},'=S.',f{k},'(els,:);']);
+        S1.(f{k})=S.(f{k})(els,:);
     else
-        eval(['S1.', f{k},'=S.', f{k},'(els);']);
+        %eval(['S1.', f{k},'=S.', f{k},'(els);']);
+        S1.(f{k})=S.(f{k})(els);
     end      
 end
