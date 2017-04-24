223616f5815565aadd0c863ca26117944dc70bf6
Ben Smith <bsmith@apl.washington.edu>
2017-04-24 09:46:29 -0700

    Replaced ad-hoc poisson-random-variable routine with a proper formula based on the inverse CDF

--- poisson_rv.m
+++ poisson_rv.m
@@ -1,11 +1,13 @@
 function x=poisson_rv(lambda, N)
 
-% want mult*lambda >>50 -> mult=50/lambda
-% want 1/mult < 1 -> mult > 1
-x=zeros(size(lambda));
-mult=max(10,50/lambda);
-if any(lambda>0);
-    x(lambda>0)=round(sum(rand(ceil(mult*lambda(lambda>0)),N)<(1/mult)));
-end
+% % want mult*lambda >>50 -> mult=50/lambda
+% % want 1/mult < 1 -> mult > 1
+% x=zeros(size(lambda));
+% mult=max(max(10,50./lambda));
+% if any(lambda>0)
+%     x(lambda>0)=round(sum(rand(ceil(mult*lambda(lambda>0)),N)<(1/mult)));
+% end
+% 
+%  
 
- 
+x=gammaincinv(randn(N,1), lambda);
