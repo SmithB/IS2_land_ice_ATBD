38f6a719a596970cb44211fe59e74155e9152395
Ben Smith <bsmith@apl.washington.edu>
2017-05-05 08:58:09 -0700

    robust_peak_width_CDF: precalculate scale factor, make XR argument mandatory

--- robust_peak_width_CDF.m
+++ robust_peak_width_CDF.m
@@ -8,9 +8,9 @@ function [sigma_hat, med]=robust_peak_width_CDF(x, N_BG, XR)
 % and last point where Ctot < 0.84 Nsig + (t1-t0)*BGR
 % the difference gives the peak width
 
-if ~exist('XR','var')
-    XR=[min(x) max(x)];
-end
+% if ~exist('XR','var')
+%     XR=[min(x) max(x)];
+% end
 BGR=N_BG/diff(XR);
 
 N_sig=length(x)-N_BG;
@@ -20,8 +20,10 @@ C=0.5:length(x)-0.5; C=C(:);
 
 %new October 2015: switch to P vals of .25 and .75, include correction
 %based on the erfinv 
-P_vals=[.25 .75];
-scale_factor=diff(erfinv(P_vals));
+ P_vals=[.25 .75];
+% scale_factor=diff(erfinv(P_vals));
+% N.B.  This is always the same, so compute by hand:
+scale_factor=.5881;
 
 i0=find(C<P_vals(1)*N_sig + (xs-XR(1))*BGR, 1, 'last'); 
 if isempty(i0); i0=1;end
