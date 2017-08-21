5a941bc54541f64ab3d37d0d348e76926ddc9653
Ben Smith <bsmith@apl.washington.edu>
2017-08-21 16:20:50 -0700

    read_ATL06_h5.m: deleted semicolon

--- read_ATL06_h5.m
+++ read_ATL06_h5.m
@@ -2,7 +2,7 @@ function [D3, Dhist]=read_ATL06_h5(h5_file)
 
 
 I=h5info(h5_file,'/');
-for kD=1:length(I.Datasets);
+for kD=1:length(I.Datasets)
     D3.(I.Datasets(kD).Name)=h5read(h5_file,['/', I.Datasets(kD).Name]);
 end
 
