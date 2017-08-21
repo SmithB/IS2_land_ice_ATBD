6310365d1b1f1abd18a92340856c4a308612d558
Ben Smith <bsmith@apl.washington.edu>
2017-08-21 16:12:45 -0700

    robust_peak_width_CDF.m: Handle the case where N_data-N_noise < 0 (in weak signal cases, happens because of Poisson noise)

--- robust_peak_width_CDF.m
+++ robust_peak_width_CDF.m
@@ -1,4 +1,4 @@
-function [sigma_hat, med]=robust_peak_width_CDF(x, N_BG, XR)
+function [sigma_hat, med, DEBUG_OUT]=robust_peak_width_CDF(x, N_BG, XR)
 
 % C_tot=C_sig+C_noise
 %      =C_sig(t)+(t-t0)BGR
@@ -13,7 +13,7 @@ function [sigma_hat, med]=robust_peak_width_CDF(x, N_BG, XR)
 % end
 BGR=N_BG/diff(XR);
 
-N_sig=length(x)-N_BG;
+N_sig=max(0,length(x)-N_BG);
 
 xs=sort(x); xs=xs(:);
 C=0.5:length(x)-0.5; C=C(:);
@@ -31,7 +31,30 @@ i1=find(C>P_vals(2)*N_sig + (xs-XR(1))*BGR, 1, 'first');
 if isempty(i1); i1=length(x);end
 sigma_hat=diff(xs([i0 i1]))/2/scale_factor;
 
-if nargout==2;
+if nargout==3
+    DEBUG_OUT.i0=i0;
+    DEBUG_OUT.i1=i1;
+    DEBUG_OUT.z0=xs(i0);
+    DEBUG_OUT.z1=xs(i1);
+    DEBUG_OUT.C_threshold_0= P_vals(1)*N_sig + (xs-XR(1))*BGR;
+    DEBUG_OUT.C_threshold_1= P_vals(2)*N_sig + (xs-XR(1))*BGR;
+end
+
+% if sigma_hat is less than zero, we have a problem--assume that the
+% interval is symmetric around the peak, report the range of the central
+% group of 0.5*Nsig points
+if sigma_hat < 0
+    i0=find(C < (0.5*length(x)+0.5-N_sig/4), 1, 'last');
+    i1=find(C > (0.5*length(x)+0.5+N_sig/4), 1, 'first');
+    sigma_hat=diff(xs([i0 i1]))/2/scale_factor;
+end
+
+if sigma_hat<0
+    disp('huh?');
+end
+
+
+if nargout>1
     i0=find(C<0.5*N_sig + (xs-XR(1))*BGR, 1, 'last');
     if isempty(i0); i0=1;end
     i1=find(C>0.5*N_sig + (xs-XR(1))*BGR, 1, 'first');
