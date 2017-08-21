4fc4ee358f4d9c960f0a48fdacdc980a5dadb9ce
Ben Smith <bsmith@apl.washington.edu>
2017-08-21 16:23:50 -0700

    write_ATL11_h5.m: minor bug cleanups

--- write_ATL11_h5.m
+++ write_ATL11_h5.m
@@ -2,16 +2,16 @@ function write_ATL11_h5(h5_file, D3b, D3a);
 
 if exist(h5_file,'file'); delete(h5_file); end
 
-
-% write out the ATL06 data that comes with this file
-ff=fieldnames(D3a);
-for kf=1:length(ff);
-    temp=D3a.(ff{kf});
-    this_field=['/ATL06/', ff{kf}];
-    h5create(h5_file, this_field, size(temp),'ChunkSize', [min(size(temp,1), 1024), 1], 'Datatype','double','Deflate', 9);
-    h5write(h5_file, this_field,  temp);
+if exist('D3a','var');
+    % write out the ATL06 data that comes with this file
+    ff=fieldnames(D3a);
+    for kf=1:length(ff);
+        temp=D3a.(ff{kf});
+        this_field=['/ATL06/', ff{kf}];
+        h5create(h5_file, this_field, size(temp),'ChunkSize', [min(size(temp,1), 1024), 1], 'Datatype','double','Deflate', 9);
+        h5write(h5_file, this_field,  double(temp));
+    end
 end
-
 % loop over the D3b structure
 f0=fieldnames(D3b);
 for k0=1:length(f0);
@@ -23,7 +23,7 @@ for k0=1:length(f0);
         this_field=['/',f0{k0},'/',ff{kf}]; 
         if ~isempty(temp)
             h5create(h5_file, this_field, size(temp),'ChunkSize', [min(size(temp,1), 1024), 1], 'Datatype','double','Deflate', 9);
-            h5write(h5_file, this_field,  temp);
+            h5write(h5_file, this_field,  double(temp));
         end
     end
 end
