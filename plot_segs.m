49ba84673d111b46f14cd344301c3408aa8bc324
Ben Smith <bsmith@apl.washington.edu>
2017-08-21 16:18:40 -0700

    plot_segs.m: return handles

--- plot_segs.m
+++ plot_segs.m
@@ -1,4 +1,4 @@
-function h=plot_segs(x, h, slope, W, linespec);
+function h=plot_segs(x, h, slope, W, linespec)
 
 
-plot([x(:)-W/2 x(:)+W/2]', [h(:)-slope(:)*W/2 h(:)+slope(:)*W/2]', linespec);
+h=plot([x(:)-W/2 x(:)+W/2]', [h(:)-slope(:)*W/2 h(:)+slope(:)*W/2]', linespec);
