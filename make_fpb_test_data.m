229f68a854e66c3975b10bd27953de89bc704966
Ben Smith <bsmith@apl.washington.edu>
2017-09-07 11:01:44 -0700

    fpb_test_data.m: generate level-3 data by default

--- make_fpb_test_data.m
+++ make_fpb_test_data.m
@@ -31,6 +31,10 @@ if ~exist([test_data_dir,'/D2'],'dir')
     mkdir([test_data_dir,'/D2']);
 end
 
+if ~exist([test_data_dir,'/D3'],'dir')
+    mkdir([test_data_dir,'/D3']);
+end
+
 [Rough,Rsurf]=meshgrid(Rough0, Rsurf0);
 
 Nsegs=2000;
@@ -71,52 +75,50 @@ for k=1:numel(Rough)
            
         write_test_file(D2_file, D2, params);
     end
-    if false
-        %TEST_CODE% [D2, params]=read_test_file(D2_file);         for beam=1:2; params(beam).WF=WF;end
-        %[D2, params]=read_test_file(D2_file);
+   
+    for kB=1:2
+        D2(kB).t_ph=D2(kB).h/(-c2);
+    end
+    delta_t=25e-12; %ATBD value
+    % make a histogram that's large enough to span the whole range of
+    % heights
+    t_WF_full=-5e-9:delta_t:5e-9;
+    signal_threshold_for_gain_corr=0.01;
+    for k0=1:max(D2(2).segment_number)-1
         for kB=1:2
-            D2(kB).t_ph=D2(kB).h/(-c2);
-        end
-        delta_t=25e-12; %ATBD value
-        % make a histogram that's large enough to span the whole range of
-        % heights
-        t_WF_full=-5e-9:delta_t:5e-9;
-        signal_threshold_for_gain_corr=0.01;
-        for k0=1:max(D2(2).segment_number)-1
-            for kB=1:2
-                these=D2(kB).segment_number==k0 | D2(kB).segment_number==k0+1;
-                tt=D2(kB).t_ph(these & D2(kB).detected);
-                % do an iterative trim on these photons (b/c fpb_corr is
-                % tested on trimmed distributions;
-                ind=true(size(tt));
-                last_ind=false(size(tt));
-                it_count=0;
-                while any(last_ind~=ind) && it_count<10
-                    last_ind=ind;
-                    tbar=mean(tt(ind));
-                    sigma=max(0.1/c2, iqr(tt(ind))/2);
-                    ind=abs(tt-tbar)<3*sigma;
-                    it_count=it_count+1;
-                end
-                
-                counts=my_histc(tt(ind)-tbar, t_WF_full);
-                [med, centroid, count, N_fpb_corr, sigma_med, sigma_centroid, minGain, gain_full]=...
-                    fpb_corr(t_WF_full, counts,  params(kB).N_channels, 58, params(kB).t_dead, signal_threshold_for_gain_corr);
-                D3.mean_uncorr(k0, kB)=tbar*(-c2);
-                D3.med_uncorr(k0, kB)=median(tt(ind))*(-c2);
-                D3.fpb_med_corr(k0, kB)=med;
-                D3.fpb_mean_corr(k0, kB)=centroid;
-                D3.count(:, k0, kB)=counts;
-                D3.count_FPB_corr(:,k0, kB)=N_fpb_corr;
-                D3.fpb_sigma_med(k0, kB)=sigma_med;
-                D3.fpb_sigma_mean(k0, kB)=sigma_centroid;
-                D3.gain(:, k0, kB)=gain_full;
-                D3.pulse_0(k0, kB)=min(D2(kB).pulse_num);
-                D3.pulse_1(k0, kB)=max(D2(kB).pulse_num);
+            these=D2(kB).segment_number==k0 | D2(kB).segment_number==k0+1;
+            tt=D2(kB).t_ph(these & D2(kB).detected);
+            % do an iterative trim on these photons (b/c fpb_corr is
+            % tested on trimmed distributions;
+            ind=true(size(tt));
+            last_ind=false(size(tt));
+            it_count=0;
+            while any(last_ind~=ind) && it_count<10
+                last_ind=ind;
+                tbar=mean(tt(ind));
+                sigma=max(0.1/c2, iqr(tt(ind))/2);
+                ind=abs(tt-tbar)<3*sigma;
+                it_count=it_count+1;
             end
+            
+            counts=my_histc(tt(ind)-tbar, t_WF_full);
+            [med, centroid, count, N_fpb_corr, sigma_med, sigma_centroid, minGain, gain_full]=...
+                fpb_corr(t_WF_full, counts,  params(kB).N_channels, 58, params(kB).t_dead, signal_threshold_for_gain_corr);
+            D3.mean_uncorr(k0, kB)=tbar*(-c2);
+            D3.med_uncorr(k0, kB)=median(tt(ind))*(-c2);
+            D3.fpb_med_corr(k0, kB)=med;
+            D3.fpb_mean_corr(k0, kB)=centroid;
+            D3.count(:, k0, kB)=counts;
+            D3.count_FPB_corr(:,k0, kB)=N_fpb_corr;
+            D3.fpb_sigma_med(k0, kB)=sigma_med;
+            D3.fpb_sigma_mean(k0, kB)=sigma_centroid;
+            D3.gain(:, k0, kB)=gain_full;
+            D3.pulse_0(k0, kB)=min(D2(kB).pulse_num);
+            D3.pulse_1(k0, kB)=max(D2(kB).pulse_num);
         end
+        
         D3_file=strrep(D2_file,'D2','D3');
-        write_struct_to_h5_file(D3_file, D3);   
+        write_struct_to_h5_file(D3_file, D3);
     end
         
 end
