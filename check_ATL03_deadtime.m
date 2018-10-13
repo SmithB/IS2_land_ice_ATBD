6bda542cffd1e264785c4007f78bd687ec8f0995
Ben Smith <bsmith@apl.washington.edu>
2018-10-12 20:06:43 -0700

    check_ATL03_deadtime.m: simplified to use fewer external routines

--- check_ATL03_deadtime.m
+++ check_ATL03_deadtime.m
@@ -1,27 +1,36 @@
-[H,D3]=read_ATL03_photon_data('ATL03_2018_10_02_00100110_944_01.h5', 1, struct);
-close all
-% loop over beams
-for kB=1:2
-    % loop over channels in the beam:
-    uI=unique(D3(1).ph_id_channel);
-    for kCh=1:length(uI)
-        % pull out the photons for the current channel
-        D=index_struct(D3(kB), D3(kB).ph_id_channel==uI(kCh));
-        % calculate the pulse number
-        D.Pnum=double(D.ph_id_pulse)+200*double(D.pce_mframe_cnt-min(D.pce_mframe_cnt));
-        % sort by pulse number, then by (negative) height
-        [~, ind]=sort(D.Pnum+(max(double(D.h_ph))+500-double(D.h_ph))/10000);
-        D=index_struct(D, ind);
-        
-        % convert height to time
-        dt=diff(double(D.h_ph)/(-1.5e8));
-        
-        % select the delta-t values from the same pulse number
-        good_dt=diff(D.Pnum)==0;
-        dt=dt(good_dt);
-        
-        % make a histogram of the dt values.
-        figure(double(uI(kCh)));
-        histogram(dt, [-0.5e-8:1e-10:0.5e-8]);
-    end
+thefile='/Volumes/ice2/ben/scf/plateau_03/ATL03_2018_10_02_00100110_944_01.h5';
+
+fields={'h_ph','ph_id_pulse','pce_mframe_cnt','ph_id_channel'};
+beam='/gt1l';
+clear D3;
+for kf=1:length(fields);
+    D3.(fields{kf})=double(h5read(thefile,[beam,'/heights/', fields{kf}]));
 end
+
+
+%[H,D3]=read_ATL03_photon_data('ATL03_2018_10_02_00100110_944_01.h5', 1, struct);
+close all
+% loop over channels in the beam:
+uI=unique(D3(1).ph_id_channel);
+for kCh=1:length(uI)
+    % pull out the photons for the current channel
+    D=index_struct(D3, D3.ph_id_channel==uI(kCh));
+    % calculate the pulse number
+    D.Pnum=double(D.ph_id_pulse)+200*(D.pce_mframe_cnt-min(D.pce_mframe_cnt));
+    % sort by pulse number, then by (negative) height
+    ph_scale=(max(D.h_ph)-min(D.h_ph))*1.05;
+    [~, ind]=sort(D.Pnum+(max(D.h_ph)-D.h_ph)/ph_scale);
+    D=index_struct(D, ind);
+    
+    % convert height to time
+    dt=diff(D.h_ph/(-1.5e8));
+    
+    % select the delta-t values from the same pulse number
+    good_dt=diff(D.Pnum)==0;
+    dt=dt(good_dt);
+    
+    % make a histogram of the dt values.
+    figure(double(uI(kCh)));
+    histogram(dt, [-0.5e-8:1e-10:0.5e-8]);
+end
+
